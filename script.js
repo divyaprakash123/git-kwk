be715aaaf3b408f5319426d32f1a316e634419a5
divyaprakash123 <kishori.divya@gmail.com>
2025-07-07 21:26:45 -0500

    fixed coloring issue with dark mode, additional styling changes, about me edits

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // added new js file
+
+// Toggling dark mode with a button at the top
+document.getElementById('dark-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+  });
